c64f611ac57dce4260a6f2507c3fc9d775468fa2
denghao <18879138231@163.com>
2020-10-31 11:08:43 +0800

    add a cpp

---
+++ 13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	fstream fin;
+fin.open("data.txt",ios::in);
+int sum=0,str;
+while(fin>>str)
+sum+=str;
+fin.close();
+fstream fout("result.txt",ios::out);
+fout<<sum;
+fout.close();
+return 0;
+ } 
